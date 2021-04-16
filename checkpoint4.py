4eac55334e7e8d92afdf2593597b3bfd7f8d5de3
Sushiman33 <joshuawildfeuer@gmail.com>
2021-04-15 21:18:15 -0400

    for OSS Lab 9

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,5 +1,27 @@
 from pymongo import MongoClient
-client = MongoClient()
+import pprint
+from bson.objectid import ObjectId
+client = MongoClient('localhost', 27017)
 
 if __name__ == '__main__':
-    print("Modify me")
+    db = client.mongo_db_lab
+    definitions = db.definitions
+
+    print("fetching all records\n")
+    for definition in definitions.find():
+        pprint.pprint(definition)
+
+    print("\n\nfetching one record\n")
+    pprint.pprint(definitions.find_one())
+    
+    print("\n\nfetching specific record (Capitaland)\n")
+    pprint.pprint(definitions.find_one({"word": "Capitaland"}))
+
+    print("\n\nfetching record by object id (Capitaland ... same as before)\n")
+    pprint.pprint(definitions.find_one({"_id": ObjectId("56fe9e22bad6b23cde07b8cb")}) )
+
+    print("\n\ninserting new record\n")
+    definitions.insert_one({'word':'dino','definition':'A magical place where BBQ and alcohol meet in holy matrimony'}) 
+    #proving word did show up
+    pprint.pprint(definitions.find_one({"word": "dino"}))
+                    
--- checkpoint5.py
+++ checkpoint5.py
@@ -1,5 +1,10 @@
 from pymongo import MongoClient
-client = MongoClient()
+from random import randint
+from datetime import datetime
+client = MongoClient('localhost', 27017)
+
+db = client.mongo_db_lab
+definitions = db.definitions
 
 
 def random_word_requester():
@@ -7,8 +12,15 @@ def random_word_requester():
     This function should return a random word and its definition and also
     log in the MongoDB database the timestamp that it was accessed.
     '''
+    index = randint(0,definitions.count_documents({})-1)
+    definitions.update_one({'word':definitions.find()[index]['word']},
+       {'$push':{'dates':str(datetime.utcnow())}})
+    doc = definitions.find()[index]
+    print("word: \"{}\"".format(doc['word']))
+    print("defintion: \"{}\"".format(doc['definition']))
+    print("dates: {}".format(', '.join(doc['dates'])))
     return
 
 
 if __name__ == '__main__':
-    print(random_word_requester())
+    random_word_requester()
